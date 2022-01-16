cfc8bc258a146f9f82af9c0b344554376b3e0125
TaylorMorini <morinitaylor0@gmail.com>
2022-01-16 17:25:57 -0500

    added comments

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -1,6 +1,7 @@
 import { html, css, LitElement } from 'lit';
 
 export class HelloWorld extends LitElement {
+  //this code is for visuals
   static get styles() {
     return css`
       :host {
@@ -10,7 +11,7 @@ export class HelloWorld extends LitElement {
       }
     `;
   }
-
+//(below) I think is to declare the types but I have not used this syntax before
   static get properties() {
     return {
       title: { type: String },
@@ -18,16 +19,21 @@ export class HelloWorld extends LitElement {
     };
   }
 
+//This constructor is calling super(), which is going to be the parent class
+//basically, it allows access to the parent class
+//after that the "this.title" and "this.counter" are just setting default values
   constructor() {
     super();
     this.title = 'Hey there';
     this.counter = 5;
   }
-
+//this is incrementing the counter that was already defined by 1
   __increment() {
     this.counter += 1;
   }
-
+//this is responsible for rendering the code in html format
+//the "<h2> tags are the html tags, they're displaying the counter/the title"
+//the "<button> tags display a button/act as event listeners. When they are clicked the counter is incremented."
   render() {
     return html`
       <h2>${this.title} Nr. ${this.counter}!</h2>
